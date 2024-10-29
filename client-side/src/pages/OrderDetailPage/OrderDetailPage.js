f3a33821fab5ef18b1256fdfd5f9aae0220c5f24
senkiealkan <22520008@gm.uit.edu.vn>
2024-10-29 23:37:46 +0700

    Update OrderDetailPage

--- client-side/src/pages/OrderDetailPage/OrderDetailPage.js
+++ client-side/src/pages/OrderDetailPage/OrderDetailPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import './OrderDetailPage.css';
 import Footer from '~/layouts/Footer/Footer';
 import Header from '~/layouts/Header/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTruckFast, faReceipt, faBox, faCheck, faCircleXmark} from '@fortawesome/free-solid-svg-icons'
+
 // Dữ liệu mẫu giả lập nhiều đơn hàng
 const ordersData = [
   {
     id: '1111',
-    date: '10/09/2023 - 09:00',
-    status: 'Hoàn tất',
+    date: '20/09/2023 - 09:00',
+    events: [
+      { status: 'Đơn hàng mới', time: '20/09/2023 - 09:00', isCompleted: true, icon: faReceipt},
+      { status: 'Đang xử lý', time: '', isCompleted: true, icon: faBox},
+      { status: 'Hoàn tất', time: '', isCompleted: false, icon: faCheck},
+    ],
     recipient: {
       name: 'Tuyền Qua Minh Nhân',
       phone: '0123456789',
@@ -23,108 +31,95 @@ const ordersData = [
     packages: [
       {
         id: 1,
-        trackingNumber: '402176646',
-        status: 'Hoàn tất',
+        trackingNumber: '1111',
         products: [
           { name: 'Thám Tử Lừng Danh Conan - Tập 103', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/103_8b03a2f691474a01bea7c3b7b7010519_master.jpg' },
           { name: 'Thám Tử Lừng Danh Conan - Tập 102', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/102_359d8e4410484914b3ccee5831926b14_master.jpg' },
           { name: 'Thám Tử Lừng Danh Conan - Tập 101', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/101_2c49d5bcea3d4b2e908c48550c1e0a39.jpg' },
         ],
         events: [
-          { status: 'Đang xác nhận', time: '10/09/2023 - 09:00' },
-          { status: 'Đang xử lý', time: '10/09/2023 - 12:00' },
-          { status: 'Hoàn tất', time: '11/09/2023 - 10:00' },
+          { status: 'Đang xác nhận', time: '20/09/2023 - 09:00', isCompleted: true, icon: faReceipt},
+          { status: 'Đang xử lý', time: '', isCompleted: true, icon: faBox },
+          { status: 'Đang vận chuyển', time: '', isCompleted: false, icon: faTruckFast },
+          { status: 'Hoàn tất', time: '',isCompleted: false, icon: faCheck },
         ],
       },
     ],
   },
+  // 2
   {
-    id: '222222222',
-    date: '9/09/2023 - 09:00',
-    status: 'Đang xử lý',
+    id: '2222',
+    date: '11/09/2023 - 00:00',
+    events: [
+      { status: 'Đơn hàng mới', time: '10/09/2023 - 00:00', isCompleted: true, icon: faReceipt},
+      { status: 'Đang xử lý', time: '10/09/2023 - 12:00', isCompleted: true, icon: faBox},
+      { status: 'Hoàn tất', time: '12/09/2023 - 14:30', isCompleted: true, icon: faCheck},
+    ],
     recipient: {
-      name: 'Tuyền Qua Minh Nhân',
+      name: 'Ái Lạc Khắc Nhĩ Tư',
       phone: '0123456789',
       address: 'Đường làng, Thủ Đức, TP.HCM',
     },
-    paymentMethod: 'Chuyển khoản ngân hàng',
+    paymentMethod: 'Tiền mặt',
     totalAmount: {
-      subtotal: 30000,
+      subtotal: 90000,
       shippingFee: 10000,
-      total: 40000,
+      total: 100000,
     },
     shippingMethod: 'Giao hàng tiêu chuẩn',
     packages: [
       {
         id: 1,
-        trackingNumber: '222222222',
-        status: 'Đang vận chuyển',
+        trackingNumber: '2222',
         products: [
           { name: 'Thám Tử Lừng Danh Conan - Tập 103', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/103_8b03a2f691474a01bea7c3b7b7010519_master.jpg' },
+          { name: 'Thám Tử Lừng Danh Conan - Tập 102', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/102_359d8e4410484914b3ccee5831926b14_master.jpg' },
+          { name: 'Thám Tử Lừng Danh Conan - Tập 101', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/101_2c49d5bcea3d4b2e908c48550c1e0a39.jpg' },
         ],
         events: [
-          { status: 'Đang xác nhận', time: '10/09/2023 - 09:00' },
-          { status: 'Đang xử lý', time: '10/09/2023 - 12:00' },
-          { status: 'Hoàn tất', time: '11/09/2023 - 10:00' },
-        ],
-      },
-      {
-        id: 2,
-        trackingNumber: '222222222',
-        status: 'Đang xử lý',
-        products: [
-          { name: 'Thám Tử Lừng Danh Conan - Tập 103', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/103_8b03a2f691474a01bea7c3b7b7010519_master.jpg' },
-        ],
-        events: [
-          { status: 'Đang xác nhận', time: '10/09/2023 - 10:00' },
-          { status: 'Đang xử lý', time: '10/09/2023 - 14:00' },
-          { status: 'Hoàn tất', time: '11/09/2023 - 12:00' },
+          { status: 'Đang xác nhận', time: '10/09/2023 - 09:00', isCompleted: true, icon: faReceipt},
+          { status: 'Đang xử lý', time: '10/09/2023 - 12:00', isCompleted: true, icon: faBox },
+          { status: 'Đang vận chuyển', time: '', isCompleted: true, icon: faTruckFast },
+          { status: 'Hoàn tất', time: '12/09/2023 - 14:30',isCompleted: true, icon: faCheck },
         ],
       },
     ],
   },
+  ////3
   {
-    id: '333333333',
-    date: '10/09/2023 - 09:00',
-    status: 'Bị hủy',
+    id: '3333',
+    date: '9/9/2023 - 09:00',
+    events: [
+      { status: 'Đơn hàng mới', time: '9/9/2023 - 09:00', isCompleted: true, icon: faReceipt},
+      { status: 'Đang xử lý', time: '', isCompleted: true, icon: faBox},
+      { status: 'Bị hủy', time: '9/9/2023 - 10:00', isCompleted: true, icon: faCircleXmark},
+    ],
     recipient: {
-      name: 'Tuyền Qua Minh Nhân',
+      name: 'Khục...khục..khụ khụ',
       phone: '0123456789',
       address: 'Đường làng, Thủ Đức, TP.HCM',
     },
     paymentMethod: 'Chuyển khoản ngân hàng',
     totalAmount: {
-      subtotal: 60000,
+      subtotal: 90000,
       shippingFee: 10000,
-      total: 70000,
+      total: 100000,
     },
     shippingMethod: 'Giao hàng tiêu chuẩn',
     packages: [
       {
         id: 1,
-        trackingNumber: '333333333',
-        status: 'Bị hủy',
+        trackingNumber: '3333',
         products: [
           { name: 'Thám Tử Lừng Danh Conan - Tập 103', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/103_8b03a2f691474a01bea7c3b7b7010519_master.jpg' },
           { name: 'Thám Tử Lừng Danh Conan - Tập 102', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/102_359d8e4410484914b3ccee5831926b14_master.jpg' },
+          { name: 'Thám Tử Lừng Danh Conan - Tập 101', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/101_2c49d5bcea3d4b2e908c48550c1e0a39.jpg' },
         ],
         events: [
-          { status: 'Đang xác nhận', time: '10/09/2023 - 09:00' },
-          { status: 'Đang xử lý', time: '10/09/2023 - 12:00' },
-          { status: 'Hoàn tất', time: '11/09/2023 - 10:00' },
-        ],
-      },
-      {
-        id: 2,
-        trackingNumber: '333333333',
-        status: 'Bị hủy',
-        products: [
-          { name: 'Thám Tử Lừng Danh Conan - Tập 102', price: 30000, quantity: 1, image: 'https://product.hstatic.net/200000343865/product/102_359d8e4410484914b3ccee5831926b14_master.jpg' },
-        ],
-        events: [
-          { status: 'Đang xác nhận', time: '10/09/2023 - 10:00' },
-          { status: 'Đang xử lý', time: '10/09/2023 - 14:00' },
-          { status: 'Hoàn tất', time: '11/09/2023 - 12:00' },
+          { status: 'Đang xác nhận', time: '9/9/2023 - 09:00', isCompleted: true, icon: faReceipt},
+          { status: 'Đang xử lý', time: '', isCompleted: true, icon: faBox },
+          { status: 'Đang vận chuyển', time: '', isCompleted: true, icon: faTruckFast },
+          { status: 'Bị hủy', time: '9/9/2023 - 10:00',isCompleted: true, icon: faCircleXmark },
         ],
       },
     ],
@@ -134,47 +129,53 @@ const ordersData = [
 // Hàm lấy màu theo trạng thái
 const getStatusColor = (status) => {
   switch (status) {
+    case 'Đơn hàng mới' :    
+    case 'Đang xác nhận':
     case 'Đang xử lý':
-      return 'blue';
     case 'Đang vận chuyển':
-      return 'blue';
+      return 'order-theme-blue';
     case 'Hoàn tất':
-      return 'green';
+      return 'order-theme-green';
     case 'Bị hủy':
-      return 'orange';
+      return 'order-theme-orange';
     default:
-      return 'gray';
+      return 'order-theme-gray';
   }
 };
 
 const OrderDetailPage = () => {
-  const steps = [
-    { label: 'Đang xác nhận', key: 'processing' },
-    { label: 'Đang xử lý', key: 'processing' },
-    { label: 'Hoàn tất', key: 'completed' },
-  ];
-
   return (
     <>
     <Header/>
     <main className="order-detail-container">
     <h2 className='order-detai-page-header'>Đơn hàng của tôi</h2>
       {ordersData.map((order) => {
-        //const currentStepIndex = steps.findIndex((step) => step.key === order.status);
-
+        const completedOrderEvents = order.events.filter(event => event.isCompleted);
+        const currentOrderStatus = completedOrderEvents[completedOrderEvents.length - 1].status;
         return (
           <div key={order.id} className="order">
             <div className="order-only">
             {/* Mã đơn hàng và ngày mua */}
             <div className="order-header">
-              <h1>Mã đơn hàng #{order.id}</h1>
-              <span className={`status ${getStatusColor(order.status)}`}>{order.status}</span>
-              <p className="order-date">Ngày mua: {order.date}</p>
+            <div className="order-header-left">
+              <h1 className = "order-id">Mã đơn hàng #{order.id}</h1>
+              <span className={`status ${getStatusColor(currentOrderStatus)}`}>{currentOrderStatus}</span>
+            </div>
+
+                <p className="order-date">Ngày mua: {order.date}</p>
             </div>
 
             {/* Thanh tiến trình tổng đơn hàng */}
-            <div className="progress-bar">
-              
+            <div className={`progress-bar ${getStatusColor(currentOrderStatus)}`}>
+            {
+              order.events.map((step,i)=>(
+                  <div key={i} className={`step-item ${step.isCompleted ? getStatusColor(currentOrderStatus) : ''}`}>
+                  <FontAwesomeIcon className="status-icon" icon={step.icon}/>
+                  <p>{step.status}</p>   
+                  <p>{step.time}</p>     
+                </div>
+              ))
+            }
             </div>
 
             {/* Thông tin người nhận, thanh toán, và tổng tiền */}
@@ -195,25 +196,55 @@ const OrderDetailPage = () => {
                 <p>Phí vận chuyển: {order.totalAmount.shippingFee.toLocaleString('vi-VN')} đ</p>
                 <h2>Tổng số tiền (gồm VAT): {order.totalAmount.total.toLocaleString('vi-VN')} đ </h2>
               </div>
-            </div>          
+            </div>     
+
+            {/* Các nút tùy thuộc vào trạng thái đơn hàng */}
+            <div className="order-actions">
+              {currentOrderStatus === "Đơn hàng mới" || currentOrderStatus === "Đang xử lý" ? (
+                <>
+                  <button className="btn-cancel-order">Hủy đơn</button>
+                  <button className="btn-reorder">Mua lại</button>
+                </>
+              ) : currentOrderStatus === "Hoàn tất" ? (
+                <>
+                  <button className="btn-review-order">Đánh giá đơn hàng</button>
+                  <button className="btn-reorder">Mua lại</button>
+                </>
+              ) : currentOrderStatus === "Bị hủy" ? (
+                <button className="btn-reorder">Mua lại</button>
+              ) : null}
+            </div>
             </div>
             <div className="shipping-method-box">
                 <h2>Phương thức vận chuyển</h2>
                 <p>{order.shippingMethod} </p>
             </div>
+
             {/* Kiện hàng */}
-            {order.packages.map((pkg) => (
+            {order.packages.map((pkg) => {
+            const completedPkgEvents = pkg.events.filter(event => event.isCompleted);
+            const currentPkgStatus = completedPkgEvents[completedPkgEvents.length - 1].status;
+            return (
               <div className="package-only">
               <div key={pkg.id} className="package">
                 <div className="package-header">
                   <h2>Kiện hàng #{pkg.id} | Mã đơn hàng: {pkg.trackingNumber}</h2>
-                  <a href="" className="shipping-lookup">Tra cứu vận chuyển</a>
-                  <span className={`status ${getStatusColor(pkg.status)}`}>{pkg.status}</span>
+                  <div className="package-header-right">
+                    <a href="" className="shipping-lookup">Tra cứu vận chuyển</a>
+                    <span className={`status ${getStatusColor(currentPkgStatus)}`}>{currentPkgStatus}</span>
+                  </div>
                 </div>
 
                 {/* Thanh tiến trình của kiệns hàng */}
-                <div className="progress-bar">
-
+                <div className={`progress-bar ${getStatusColor(currentPkgStatus)}`}>
+                {
+                  pkg.events.map((step,i)=>(
+                    <div key={i} className={`step-item ${step.isCompleted ? getStatusColor(currentPkgStatus) : ''}`}>
+                    <FontAwesomeIcon className="status-icon" icon={step.icon}/>
+                    <p>{step.status}</p>   
+                    <p>{step.time}</p>     
+                  </div>
+                ))}
                 </div>
 
                 {/* Sản phẩm trong kiện hàng */}
@@ -236,11 +267,18 @@ const OrderDetailPage = () => {
                       </div>
                     </div>
                   ))}
-                  {/* Tổng tiền  */}
+                  <div className="total-product-row">
+                    <span className="total-label">Tổng tiền: </span>
+                    <span className="total-amount">
+                    {pkg.products
+                        .reduce((total, product) => total + product.price * product.quantity, 0)
+                        .toLocaleString('vi-VN')} đ
+                    </span>
+                  </div>
                 </div>
               </div>
               </div>
-            ))}
+            )})}
                    
 
           </div>
